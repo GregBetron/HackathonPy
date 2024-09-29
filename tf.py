c9ab9186bfc704fa4b66a4106bf066d72964f42c
GregBetron <gregb4789@gmail.com>
2024-09-28 20:26:59 -0400

    added img and gemini

--- tf.py
+++ tf.py
@@ -1,4 +1,16 @@
-import tensorflow as tf
+#import tensorflow as tf
 import numpy as np
-import aikey
+import aikey    #secure ai key from Google Gemini
+import os
+import google.generativeai as genai
 
+genai.configure(api_key=os.environ[aikey])
+
+file = genai.upload_file(media / "papertrash.jpg")
+print(f"{file=}")
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+result = model.generate_content(
+    [file, "\n\n", "Can you tell me about what object is in this photo?"]
+)
+print(result.text)
